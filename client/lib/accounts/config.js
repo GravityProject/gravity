0a8f39f890f117634f93078f34cf5b9a2d8e8775
Philipp Muens <raute1337@gmx.de>
2015-11-12 21:03:59 +0100

    Refactor useraccounts code
    Refactor useraccounts code so that the code sticks to the
    overall used coding conventions.

--- client/lib/accounts/config.js
+++ client/lib/accounts/config.js
@@ -1,65 +1,54 @@
-/*
- * Useraccounts configuration.
- *
- */
 AccountsTemplates.configure({
-    defaultLayout: 'layout',
-    defaultLayoutRegions: {
-        header: 'header'//,
-        //footer: '_footer'
+  defaultLayout: 'layout',
+  defaultLayoutRegions: {
+    header: 'header'//,
+    //footer: '_footer'
+  },
+  defaultContentRegion: 'main',
+  showForgotPasswordLink: true,
+  overrideLoginErrors: true,
+  enablePasswordChange: true,
+  sendVerificationEmail: true,
+  lowercaseUsername: false,
+
+  //enforceEmailVerification: true,
+  confirmPassword: true,
+  continuousValidation: true,
+  //displayFormLabels: true,
+  //forbidClientAccountCreation: false,
+  homeRoutePath: '/',
+  //showAddRemoveServices: false,
+  showPlaceholders: true,
+
+  negativeValidation: true,
+  positiveValidation: true,
+  negativeFeedback: false,
+  positiveFeedback: false,
+
+  //privacyUrl: 'privacy',
+  //termsUrl: 'terms',
+
+  texts: {
+    title: {
+      changePwd: 'Change password',
+      forgotPwd: 'Forgot password?',
+      resetPwd: 'Reset password',
+      signIn: 'Sign in',
+      signUp: 'Sign up',
+      verifyEmail: 'Verify Email'
     },
-    defaultContentRegion: 'main',
-    showForgotPasswordLink: true,
-    overrideLoginErrors: true,
-    enablePasswordChange: true,
-    sendVerificationEmail: true,
-    lowercaseUsername: false,
-
-    //enforceEmailVerification: true,
-    confirmPassword: true,
-    continuousValidation: true,
-    //displayFormLabels: true,
-    //forbidClientAccountCreation: false,
-    homeRoutePath: '/',
-    //showAddRemoveServices: false,
-    showPlaceholders: true,
-
-    negativeValidation: true,
-    positiveValidation: true,
-    negativeFeedback: false,
-    positiveFeedback: false,
-
-    // Privacy Policy and Terms of Use
-    // privacyUrl: 'privacy',
-    // termsUrl: 'terms',
-
-    // Form texts
-    texts: {
-      title: {
-        changePwd: "Change password",
-        forgotPwd: "Forgot password?",
-        resetPwd: "Reset password",
-        signIn: "Sign in",
-        signUp: "Sign up",
-        verifyEmail: "Verify Email",
-      },
-      button: {
-        changePwd: "Change password",
-        enrollAccount: "Enroll Text",
-        forgotPwd: "Send reset link",
-        resetPwd: "Reset Password",
-        signIn: "Sign in",
-        signUp: "Sign up",
-      }
+    button: {
+      changePwd: 'Change password',
+      enrollAccount: 'Enroll Text',
+      forgotPwd: 'Send reset link',
+      resetPwd: 'Reset Password',
+      signIn: 'Sign in',
+      signUp: 'Sign up'
     }
+  }
 });
 
-/*
- * Configuring useraccounts for login
- * with both username or email.
- *
- * https://github.com/meteor-useraccounts/core/blob/master/Guide.md#login-with-username-or-email
- */
+// configuring useraccounts for login with both username or email
 let pwd = AccountsTemplates.removeField('password');
 AccountsTemplates.removeField('email');
 AccountsTemplates.addFields([
@@ -67,35 +56,32 @@ AccountsTemplates.addFields([
     _id: 'email',
     type: 'email',
     required: true,
-    displayName: "Email",
+    displayName: 'Email',
     re: /.+@(.+){2,}\.(.+){2,}/,
-    errStr: 'Invalid email',
+    errStr: 'Invalid email'
   },
   {
-    _id: "username",
-    type: "text",
-    displayName: "Username",
+    _id: 'username',
+    type: 'text',
+    displayName: 'Username',
     required: true,
-    minLength: 4,
+    minLength: 4
   },
   {
     _id: 'username_and_email',
     placeholder: 'Username or email',
     type: 'text',
     required: true,
-    displayName: "Login",
+    displayName: 'Login'
   },
   pwd
 ]);
 
-/*
- * Enable preconfigured Flow-Router routes by useraccounts:flow-router.
- *
- */
+// enable preconfigured Flow-Router routes by useraccounts:flow-router.
 AccountsTemplates.configureRoute('changePwd');
 AccountsTemplates.configureRoute('forgotPwd');
 AccountsTemplates.configureRoute('resetPwd');
 AccountsTemplates.configureRoute('signIn');
 AccountsTemplates.configureRoute('signUp');
 AccountsTemplates.configureRoute('verifyEmail');
-// AccountsTemplates.configureRoute('enrollAccount'); // for creating passwords after logging first time
+//AccountsTemplates.configureRoute('enrollAccount'); // for creating passwords after logging first time
