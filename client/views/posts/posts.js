b4b0c3edfd0e18f3407383ba85765fa5c84782ff
Philipp Muens <raute1337@gmx.de>
2015-11-12 20:55:37 +0100

    Refactor displaying of posts on post page
    Refactor displaying of posts on post page so that the shared
    post template is reused.

--- client/views/posts/posts.js
+++ client/views/posts/posts.js
@@ -16,22 +16,6 @@ Template.posts.events({
     });
   },
 
-  'click [data-id=remove-post]': function (event, template) {
-    let post = {
-      _id: this._id
-    };
-
-    if (confirm('do you really want to remove this post? This can not be undone!')) {
-      Meteor.call('posts.remove', post, (error, result) => {
-        if (error) {
-          Bert.alert(error.reason, 'danger', 'growl-top-right');
-        } else {
-          Bert.alert('Post successfully removed', 'success', 'growl-top-right');
-        }
-      });
-    }
-  },
-
   'click [data-id=clear-form]': (event, template) => {
     template.find('[data-id=body]').value = '';
   },
@@ -56,14 +40,6 @@ Template.posts.helpers({
       return Posts.find({}, { sort: [['score', 'desc']] });
     }
     return Posts.find({}, { sort: { createdAt: -1 } });
-  },
-
-  author: function () {
-    return Meteor.users.findOne({ _id: this.authorId });
-  },
-
-  belongsPostToUser: function () {
-    return this.authorId === Meteor.userId();
   }
 });
 
