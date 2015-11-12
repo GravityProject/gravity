696219c99dd49b51bc91a1bf8fb9b66a80a72637
Philipp Muens <raute1337@gmx.de>
2015-11-12 18:13:47 +0100

    Add authorization checks to method calls
    Add authorization checks to method calls so that not signed
    in users can not call methods at all.

--- collections/users.js
+++ collections/users.js
@@ -4,6 +4,10 @@ Meteor.methods({
       biography: String
     });
 
+    if (!Meteor.user()) {
+      throw new Meteor.Error(401, 'You need to be signed in to continue');
+    }
+
     Meteor.users.update({ _id: Meteor.userId() }, { $set: { biography: user.biography } } )
   }
 });
