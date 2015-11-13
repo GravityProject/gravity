236e05ad2f96d1897fced16403b3d02066eca5de
Philipp Muens <raute1337@gmx.de>
2015-11-13 12:28:47 +0100

    Refactor method parameters
    Refactor method parameters so that plain parameters are used
    when possible instead of object based parameters.

--- client/views/shared/_post.js
+++ client/views/shared/_post.js
@@ -1,11 +1,7 @@
 Template.post.events({
   'click [data-id=remove-post]': function (event, template) {
-    let post = {
-      _id: this._id
-    };
-
     if (confirm('do you really want to remove this post? This can not be undone!')) {
-      Meteor.call('posts.remove', post, (error, result) => {
+      Meteor.call('posts.remove', this._id, (error, result) => {
         if (error) {
           Bert.alert(error.reason, 'danger', 'growl-top-right');
         } else {
