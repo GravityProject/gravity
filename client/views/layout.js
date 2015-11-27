abd5ad88c1c5a63940277bf81ca95671e4910a2a
NgoChenBang <chenbang.ngo@gmail.com>
2015-11-27 23:21:28 +0800

    Closes #76 - Add dynamic height to container

---
+++ client/views/layout.js
@@ -0,0 +1,19 @@
+let getHeight = () => {
+  let windowHeight = $(window).height(),
+      headerHeight = $('.main-header').outerHeight(true),
+      footerHeight = $('.footer').outerHeight(true);
+
+   return `${windowHeight - headerHeight - footerHeight}px`;
+}
+
+Template.layout.onCreated(function () {
+  this.minHeight = new ReactiveVar(0);
+});
+
+Template.layout.onRendered(function () {
+  this.minHeight.set(getHeight());
+
+  $(window).resize(() => {
+    this.minHeight.set(getHeight());
+  })
+});
